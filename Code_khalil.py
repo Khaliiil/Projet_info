9331faea96a96eeec27f6d765ff2824ce2fbcf9e
Khaliiil <k.mahfoudh@hotmail.fr>
2016-10-26 14:51:46 +0200

    Methode Euler par Khalil

---
+++ Code_khalil.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+import matplotlib.pyplot as plt
+import numpy as np
+
+def Euler(F, t0, tf, y0, n) :
+    t = t0
+    y = y0
+    h = (tf-t0)/float(n)
+    temps = [t0]
+    fonction = [y0]
+    for i in range(n) :
+        y = y + h*F(t, y)
+        t = t+h
+        temps.append(t)
+        fonction.append(y)
+    plt.plot(temps, fonction)
+    return fonction
+    
+def F(t, y):
+    y = -3*y
+    return y
+    
+    
+Euler(F, 0, 5, 1, 1000)
+
+#x = np.linspace(0, 5, 5)
+#y = np.exp(x)
+#plt.plot(x, y)
+ 
