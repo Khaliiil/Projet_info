d58a24f477d4dee14acf7e547d26e25da9c02aae
Khaliiil <k.mahfoudh@hotmail.fr>
2016-10-29 13:23:52 +0200

    Methode Euler par Khalil

--- Code_khalil.py
+++ Code_khalil.py
@@ -1,27 +1,66 @@
 # -*- coding: utf-8 -*-
-import matplotlib.pyplot as plt
 import numpy as np
+#import matplotlib as plt
 
-def Euler(F, t0, tf, y0, n) :
-    t = t0
-    y = y0
-    h = (tf-t0)/float(n)
-    temps = [t0]
-    solution = [y0]
-    for i in range(n) :
-        y = y + h*F(t, y)
-        t = t+h
-        temps.append(t)
-        solution.append(y)
-    plt.plot(temps, solution)
-    return solution
+
+class ODE_1():
     
-def F(t, y):# Codage de l'équation différentielle y'(t) = -3y(t)
+    def __init__(self, fonction, CI):
+        self.f = fonction
+        self.y_0 = CI
+
+    def Euler(self, t, N) :
+        
+        alpha = int(raw_input("Entrez alpha : "))
+        beta = int(raw_input("Entrez beta : "))
+        gamma = int(raw_input("Entrez gamma : "))
+        delta = int(raw_input("Entrez delta : "))
+        t0 = 0
+        h = (t-t0)/float(N)
+        t = t0
+        y = self.y_0[1]
+        x = self.y_0[0]
+        z = [x, y]
+        temps = [t0]
+        fonction1 = [x]
+        fonction2 = [y]
+        for i in range(N) :
+            x = x + h*self.f(z, alpha, beta, gamma, delta)[0]
+            y = y + h*self.f(z, alpha, beta, gamma, delta)[1]
+            z = [x, y]
+            t = t+h
+            temps.append(t)
+            fonction1.append(x)
+            fonction2.append(y)
+        #plt.plot(temps, fonction1)
+        #plt.plot(temps, fonction2) 
+        return fonction1
+    
+    def Runge_Kutta(t, N):
+        pass
+    
+
+    
+def F(x, y):
     y = -3*y
     return y
     
-    
-Euler(F, 0, 5, 1, 1000)
+#Nous allons ici tenter de définir la fonction qui représente notre sys. différentiel avec des valeurs prises au hasard pour les coeffs
 
-
+# x est une liste
+def P_P(x, alpha, beta, gamma, delta):
+    liste = list()
+    partie1 = alpha*x[0] - beta*x[0]*x[1]
+    partie2 = -gamma*x[1] + delta*x[0]*x[1]
+    liste.append(partie1)
+    liste.append(partie2)
+    return liste
  
+   
+    
+A = ODE_1(P_P, [5, 3])
+A.Euler(5, 1000)
+
+#x = np.linspace(0, 5, 5)
+#y = np.exp(x)
+#plt.plot(x, y)
